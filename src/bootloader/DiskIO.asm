cfabe6952e1f8c0e7d3fa85bcf392c605bd7274b
NowEwo <yannnonoficiel@gmail.com>
2024-11-19 18:47:09 +0100

    Some minor changes

--- src/bootloader/DiskIO.asm
+++ src/bootloader/DiskIO.asm
@@ -1,33 +1,26 @@
-disk_read:
-    ; Inputs: AX = sector number, ES:BX = buffer address
-    ; Description: Reads a single sector from the disk
-    push ax         ; Save AX
-    push bx         ; Save BX
-    push cx         ; Save CX
-    push dx         ; Save DX
+// Program that can read sectors from disk
+// EwoFluffy 2024
 
-    mov ah, 0x02    ; BIOS read sector function
-    mov al, 0x01    ; Read 1 sector
-    mov ch, 0x00    ; Cylinder number (high bits in CH)
-    mov cl, [sector_low] ; Sector number (low bits in CL)
-    mov dh, 0x00    ; Head number
-    mov dl, 0x80    ; Drive number (e.g., 0x80 for the first hard drive)
-    int 0x13        ; Call BIOS interrupt to read disk
+ReadDisk:
+    pusha ; Save registers
+    push dx
 
-    jc .error       ; Jump if carry flag is set (error occurred)
+    mov ah, 02h ; Read sector (can be 0x02 too)
+    mov al, dl ; Number of sectors to read
+    mov cl, 0x02 ; Cylinder number (0x01 is boot so we skip it)
+    mov ch, 0x00 ; Track number
+    mov dh, 0x00 ; Head number
 
-    pop dx          ; Restore DX
-    pop cx          ; Restore CX
-    pop bx          ; Restore BX
-    pop ax          ; Restore AX
-    ret             ; Return from function
+    int 0x13 ; Call BIOS
+    jc ReadDiskError ; If carry flag is set, error
 
-.error:
-    mov si, DiskErrorMsg ; Load the error message
-    call prints          ; Print the error message
-    jmp .halt            ; Halt the system
-
-.halt:
+    pop dx
+    popa ; Restore registers
     ret
 
-DiskErrorMsg db 'Disk Read Error!', LINE_END, 0
+ReadDiskError:
+    mov si, ErrorMessage ; Print error message
+    call PrintString
+    jmp $ ; Infinite loop
+
+ErrorMessage db "ReadDisk: Error reading disk", 0x0A, 0x0D, 0
--- src/bootloader/Main.asm
+++ src/bootloader/Main.asm
@@ -1,17 +1,17 @@
 org 0x7C00
 bits 16
 
-%define LINE_END 0x0D, 0x0A
+%define LINE_END 0x0A, 0x0D
 
 jmp start
 nop
 
+sector_low db 1 ; Define sector number for `disk_read`
+
 %include "src/bootloader/FileSystemDefine.asm"
-%include "src/bootloader/DiskIO.asm"
+; %include "src/bootloader/DiskIO.asm"
 %include "src/bootloader/Prints.asm"
 %include "src/bootloader/KernelLoader.asm"
-
-sector_low db 1 ; Define sector number for `disk_read`
 
 start:
     ; Bootloader initialization code
@@ -30,17 +30,21 @@ start:
     mov si, Greetings
     call prints
 
-    ; Call kernel loader
-    call load_kernel
+    mov ah,2 ; Bios service to cursor move mode
+    mov dl,0 ; Column
+    mov dh,1 ; Row
+    int 0x10
 
-    ; Jump to kernel
-    jmp 0x0000:0x1000
+    ; Print a loading message
+    mov si, BootingMessage
+    call prints
 
 .halt:
     hlt
     jmp .halt
 
-Greetings db 'Loading EwOS Kernel...', LINE_END, 0
+Greetings db 'EwOS Kernel | Ewo (c) 2024', LINE_END
+BootingMessage db 'Booting the kernel...', LINE_END
 
 times 510-($-$$) db 0
 dw 0xAA55
